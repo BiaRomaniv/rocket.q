e719399d6c1155b1d1facc21c317725b35bdb6dc
BiaRomaniv <biancaromaniv@hotmail.com>
2022-02-24 20:38:45 -0300

    abrir modal e alterar descrições internas

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,30 @@
-import Modal  from './modal.js'
+import  Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalQuestion = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
 
-//botoes com a class check
-const checkButtons = document.querySelectorAll(".actions a.check")
+//seleciona todos os botoes com a class check e delete
+const checkButtons = document.querySelectorAll("a.check")
+const deleteButtons = document.querySelectorAll("a.delete")
 
-//quando o marcar como lido for clicado
+//quando o marcar como lido for clicado abrir modal
 checkButtons.forEach(button => {
-    //adicionar a escuta
-    button.addEventListener("click", event => {
-        modal.open()
+    button.addEventListener("click", handleClick)
     })
-})
+//quando o excluir for clicado abrir modal
+deleteButtons.forEach(button => {
+    button.addEventListener("click", (event) => handleClick (event, false))
+    })
 
+//função para lidar com o click nos botoes, colocando um check em true inicialmente
+function handleClick(event, check = true) {
+    event.preventDefault() //verificar funcionalidade!
+    modalTitle.innerHTML = check ? "Marcar como lida" : "Excluir pergunta" //se check é true, troca o innerHtml pra marcar como lida, se não, Excluir
+    modalQuestion.innerHTML = check ? "Deseja marcar esta pergunta como lida?" : "Tem certeza que deseja excluir esta pergunta?"
+    modalButton.innerHTML = check ? "Sim, marcar como lida" : "Sim, excluir"
+    modal.open()
+}
 
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,20 @@
 export default function Modal() {
+
+    const cancelButtons = document.querySelectorAll('.button.cancel')// funcionalidade para fechar modal quando clicar em cancelar
+        cancelButtons.forEach(button =>{
+            button.addEventListener("click", event => {
+                close()
+            })
+        })
+
     function open(){
-        document.querySelector('.modal-wrapper').classList.add()
+        document.querySelector(".modal-wrapper").classList.add("active")// funcionalidade de atribuir a classe active do modal
 
-    } // funcionalidade de atribuir a classe active do modal
-    function close(){}// funcionalidade de excluir a classe active do modal
-
+    } 
+    function close(){
+        document.querySelector(".modal-wrapper").classList.remove("active")// funcionalidade de excluir a classe active do modal
+    }
+        
     return{
         open,
         close
