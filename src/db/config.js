cbcae2019a941b2465872c3e62fe5a9449be3466
BiaRomaniv <biancaromaniv@hotmail.com>
2022-03-07 21:22:38 -0300

    app crashed sqlite error

--- src/db/config.js
+++ src/db/config.js
@@ -1,9 +1,8 @@
-const sqlite3 = require('sqlite3');
-const {open} = require('sqlite'); /* só pega a propriedade Open do sqlite */
+const sqlite3 = require("sqlite3");
+const {open} = require("sqlite"); /* só pega a propriedade Open do sqlite */
 
-module.exports = () => {
+module.exports = () => 
     open({
         filename: './src/db/rocketq.sqlite',
         driver: sqlite3.Database
     });
-}
--- src/db/init.js
+++ src/db/init.js
@@ -1,4 +1,4 @@
-const Database = require('../db/config')
+const Database = require("../db/config")
 
 const initDb = {
     async init(){
@@ -7,17 +7,17 @@ const initDb = {
         await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
             pass TEXT
-        )`);
+        )`)
 
         await db.exec(`CREATE TABLE questions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT,
             read INT,
             room INT
-        )`);
+        )`)
 
         await db.close()
     }
 }
 
-initDb.init();
+initDb.init()
